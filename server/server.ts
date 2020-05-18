80a75fdf43f1e6f3c480aa70bc479937aa5f4b6c
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-05-18 18:49:45 -0400

    setting up an express server

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,15 @@
-export function initServer() {}
+import * as express from "express";
+import { Application } from "express";
+
+export function initServer() {
+  const app: Application = express();
+
+  app.route("/").get((req, res) => {
+    res.status(200).send("<h1>API is up and running!</h1>");
+  });
+
+  const PORT = process.env.PORT || 9000;
+  app.listen(PORT, () => {
+    console.log("HTTP TEST API Server is running at port " + PORT);
+  });
+}
